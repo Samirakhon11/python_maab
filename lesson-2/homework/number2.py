135b8fbe78738e954267657fe259f5876b3e02b1
Samirakhon11 <samirakhonjamolkhonova@gmail.com>
2025-02-09 22:56:53 +0500

    problem2

---
+++ lesson-2/homework/number2.py
@@ -0,0 +1,9 @@
+number1 = float(input("Enter the first number: "))
+number2 = float(input("Enter the second number: "))
+number3 = float(input("Enter the third number: "))
+
+largest = max(number1, number2, number3)
+smallest = min(number1, number2, number3)
+
+print("Largest number is: ", largest)
+print("Smallest number is: ", smallest)
