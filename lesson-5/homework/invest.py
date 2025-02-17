af111d76d6cf910e9fbed2a6708c82e9c7f53eef
Samirakhon11 <samirakhonjamolkhonova@gmail.com>
2025-02-18 00:01:59 +0500

    . . .

--- lesson-5/homework/invest.py
+++ lesson-5/homework/invest.py
@@ -1,7 +1,7 @@
 def invest(amount, rate, years):
     for i in range(1, years + 1):
         amount += (amount * rate)
-        print(f"year {1}: {amount:.2f}") 
+        print(f"year {i}: {amount:.2f}") 
 
 amount = int(input("Enter the initial amount of the money: "))
 rate = float(input("Enter the rate of change: "))
