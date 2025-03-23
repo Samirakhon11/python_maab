7d9c766ccc648795e38e18130122834108fd4777
Samirakhon11 <samirakhonjamolkhonova@gmail.com>
2025-03-23 22:50:45 +0500

    image updated

--- lesson-14/homework/task5.py
+++ lesson-14/homework/task5.py
@@ -1,50 +1,62 @@
-import matplotlib.pyplot as plt
-from PIL import Image
 import numpy as np
+from PIL import Image
+import cv2
 
-def flip_image(img_array):
-    flipped_h = np.fliplr(img_array)
-    flipped_v = np.flipud(img_array) 
-    return flipped_h, flipped_v 
+def flip_v_image(img_array):
+    flipped_v = cv2.flip(img_array, 0)  
+    return flipped_v
 
-def add_noise(img_array, noise_level = 30):
-    noise = np.random.randint(-noise_level, noise_level, img_array.shape(), dtype=np.int16) 
-    noise_img = np.clip(img_array + noise, 0, 255).astype(np.uint8) 
-    return noise_img
+def flip_h_image(img_array):
+    flipped_h = cv2.flip(img_array, 1)
+    return flipped_h
 
-def brighten_channels(img_array, brightness=40):
-    brightened_img = np.clip(img_array + brightness, 0, 255).astype(np.uint8)
-    return brightened_img
+img = Image.open('lesson-14/images/bird.jpg').convert("RGB") 
+img_array = np.array(img)  
 
-def apply_mask(img_array, mask_size=(100, 100)):
-    h, w, _ = img_array.shape
-    y_start, x_start = (h - mask_size[0]) // 2, (w - mask_size[1]) // 2
-    img_array[y_start:y_start + mask_size[0], x_start:x_start + mask_size[1]] = [0, 0, 0]
-    return img_array
+r, g, b = cv2.split(img_array) 
+r = np.clip(r + 40, 0, 255) 
+brightened_img = cv2.merge([r, g, b])  
 
-def show_image(img_array, title="Image"):
-    plt.figure(figsize=(5, 5))
-    plt.imshow(img_array)
-    plt.axis("off") 
-    plt.title(title)
-    plt.show()
+noise_level = 30
+noise = np.random.randint(-noise_level, noise_level, img_array.shape, dtype=np.int16)  
+noisy_img = np.clip(img_array + noise, 0, 255).astype(np.uint8)  
 
-img = Image.open('lesson-14/images/bird.jpg')
-img_array = np.array(img)
+flipped_v = flip_v_image(img_array)
+flipped_h = flip_h_image(img_array)
 
-flipped_h, flipped_v = flip_image(img_array)
-noisy_img = add_noise(img_array)
-brightened_img = brighten_channels(img_array)
-masked_img = apply_mask(img_array.copy())
+Image.fromarray(flipped_v).save("flipped_v.jpg")   
+Image.fromarray(flipped_h).save("flipped_h.jpg") 
+Image.fromarray(brightened_img).save("brightened.jpg") 
+Image.fromarray(noisy_img).save("noisy.jpg") 
 
-show_image(flipped_h, "Flipped Horizontally")
-show_image(flipped_v, "Flipped Vertically")
-show_image(noisy_img, "Noisy Image")
-show_image(brightened_img, "Brightened Image")
-show_image(masked_img, "Masked Image")
+while True:
+    print("1. Original image")
+    print("2. Flipped to the right")
+    print("3. Upside down") 
+    print("4. Brightened image")
+    print("5. Image with noise") 
+    print("6. Exit") 
 
-Image.fromarray(flipped_h).save("flipped_h.jpg")
-Image.fromarray(flipped_v).save("flipped_v.jpg")
-Image.fromarray(noisy_img).save("noisy.jpg")
-Image.fromarray(brightened_img).save("brightened.jpg")
-Image.fromarray(masked_img).save("masked.jpg")
+    try:
+        option = int(input("Which version of the picture do you want to see: "))
+        if option < 1 or option > 6:
+            raise ValueError("Enter a number between 1 and 6")
+    except ValueError as e:
+        print(e)
+        continue  
+
+    if option == 1:
+        cv2.imshow("Original image", img_array)  
+    elif option == 2:
+        cv2.imshow("Flipped to the right", flipped_h)
+    elif option == 3:
+        cv2.imshow("Upside down", flipped_v) 
+    elif option == 4:
+        cv2.imshow("Brightened image", brightened_img)
+    elif option == 5:
+        cv2.imshow("Image with noise", noisy_img) 
+    elif option == 6:
+        break
+
+    cv2.waitKey(0)  
+    cv2.destroyAllWindows()
